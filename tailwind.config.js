d8997e3b905acb774193139fbfe5da55678b3714
Tirrego <g.schmittel@gmx.de>
2024-02-21 12:17:24 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Softblue": "hsl(215, 51%, 70%)",
+        "Cyan": "hsl(178, 100%, 50%)",
+        "BlueMain": "hsl(217, 54%, 11%)",
+        "Bluecard": "hsl(216, 50%, 16%)",
+        "blueline": "hsl(215, 32%, 27%)"
+      },
+      fontFamily: {
+        "Outfit": ["Outfit", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 };
